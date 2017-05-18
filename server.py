ba830b9de25fb5f3fb54072d3fea309dfc3faca7
System <none>
2017-05-18 10:52:31 +0000

    Checkpoint

--- server.py
+++ server.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello():
   return "Hello World!"
 
+@app.route("/giuliano")
+def giuliano():
+  return "Ciao Giuliano!"
+
 if __name__ == "__main__":
+  l = [1, 2, 3, 4, 5]
+  a, b, *c = l
+  
+  print(a, b, c)
+  
   app.run()
