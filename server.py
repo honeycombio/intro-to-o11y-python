64b000dfe38212d5da5fa0b27f47d8b2e5c35f0e
Glitch (flask-python3) <none>
2018-09-19 20:56:52 +0000

    👐✌️ Checkpoint ./server.py:728660/127

--- server.py
+++ server.py
@@ -5,14 +5,5 @@ app = Flask(__name__)
 def hello():
   return "Hello World!"
 
-@app.route("/giuliano")
-def giuliano():
-  return "Ciao Giuliano!"
-
 if __name__ == "__main__":
-  l = [1, 2, 3, 4, 5]
-  a, b, *c = l
-  
-  print(a, b, c)
-  
   app.run()
