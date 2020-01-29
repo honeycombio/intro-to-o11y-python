f679b47a239967248a277efeb1cc573ca3ec4a4d
Glitch (flask-python3) <none>
2020-01-29 19:09:53 +0000

    🍄🌌 Checkpoint ./README.md:5815331/72 ./server.py:5815331/551 ./requirements.txt:5815331/19

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Python3 Flask project
+# OpenTelemetry Workshop -- Python
--- server.py
+++ server.py
@@ -1,9 +1,24 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def root():
+  return "Click [Tools] > [Logs] to see spans!"
+
+@app.route("/fib")
+def fibHandler():
+  value = request.args.get('i')
+  returnValue = 0
+  if value < 2:
+    returnValue = 1
+  else:
+    for n in range(1, 2):
+      payload = {'i': value -1}
+      resp = requests.get('http://127.0.0.1:3000/fibInternal?', payload)
+      returnValue = resp.content
+  return returnValue
 
 if __name__ == "__main__":
   app.run()
