01f30e60c3400bb27d7629209b6e391c9b324bdc
Glitch (flask-python3) <none>
2020-03-03 11:47:21 +0000

    🍞🎃 Checkpoint ./server.py:5815331/2

--- server.py
+++ server.py
@@ -32,8 +32,8 @@ exporter = JaegerSpanExporter(
   agent_port=6831,
 )
 
-trace.tracer_source().add_span_processor(SimpleExportSpanProcessor(ConsoleSpanExporter()))
-#trace.tracer_source().add_span_processor(BatchExportSpanProcessor(lsExporter))
+#trace.tracer_source().add_span_processor(SimpleExportSpanProcessor(ConsoleSpanExporter()))
+trace.tracer_source().add_span_processor(BatchExportSpanProcessor(lsExporter))
 
 tracer = trace.get_tracer(__name__)
 
