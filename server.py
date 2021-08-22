6c99935416bb04e86a8c3cbb1d2e5ab2a808fb5f
Glitch (intro-to-o11y-python) <none>
2021-08-22 15:12:26 +0000

    👭🎤 Updated with Glitch

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
+import sys
+import os
+import requests
+from flask import render_template
+from flask import Flask, request
 from opentelemetry import (
-  trace
+    trace
 )
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
-  SimpleSpanProcessor,
-  BatchSpanProcessor,
-  ConsoleSpanExporter,
+    SimpleSpanProcessor,
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
 )
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
@@ -18,11 +23,6 @@ from grpc import ssl_channel_credentials
 
 FlaskInstrumentor().instrument()
 
-from flask import Flask, request
-from flask import render_template
-import requests
-import os
-import sys
 
 resource = Resource(attributes={
     "service_name": os.getenv("SERVICE_NAME", "fibonacci-microservice")
@@ -31,16 +31,17 @@ resource = Resource(attributes={
 trace.set_tracer_provider(TracerProvider(resource=resource))
 
 hnyExporter = OTLPSpanExporter(
-	endpoint="api.honeycomb.io:443",
-	insecure=False,
-	credentials=ssl_channel_credentials(),
-	headers=(
-		("x-honeycomb-team", os.environ.get("HONEYCOMB_API_KEY")),
-		("x-honeycomb-dataset", os.getenv("HONEYCOMB_DATASET", "otel-python"))
-	)
+    endpoint="api.honeycomb.io:443",
+    insecure=False,
+    credentials=ssl_channel_credentials(),
+    headers=(
+        ("x-honeycomb-team", os.environ.get("HONEYCOMB_API_KEY")),
+        ("x-honeycomb-dataset", os.getenv("HONEYCOMB_DATASET", "otel-python"))
+    )
 )
 
-trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+trace.get_tracer_provider().add_span_processor(
+    SimpleSpanProcessor(ConsoleSpanExporter()))
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(hnyExporter))
 
 tracer = trace.get_tracer(__name__)
@@ -49,37 +50,41 @@ RequestsInstrumentor().instrument(tracer_provider=trace.get_tracer_provider())
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def root():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/fib")
 def fibHandler():
-  value = int(request.args.get('index'))
-  # TODO fix missing root span b/c of w3c header
-  # python equivalent of: othttp.WithSpanOptions(trace.WithNewRoot())
-  # or: othttp.NewHandler(othttp.WithPublicEndpoint())
-  # from workshop template slide 64
-  current_span = trace.get_current_span()
-  current_span.set_attribute("parameter", value)
-  returnValue = 0
-  if value == 0:
+    value = int(request.args.get('index'))
+    # TODO fix missing root span b/c of w3c header
+    # python equivalent of: othttp.WithSpanOptions(trace.WithNewRoot())
+    # or: othttp.NewHandler(othttp.WithPublicEndpoint())
+    # from workshop template slide 64
+    current_span = trace.get_current_span()
+    current_span.set_attribute("parameter", value)
     returnValue = 0
-  elif value == 1:
-    returnValue = 1
-  else:
-    minusOnePayload = {'index': value - 1}
-    minusTwoPayload = {'index': value - 2}
-    with tracer.start_as_current_span("get_minus_one") as span:
-      span.set_attribute("payloadValue", value-1)
-      respOne = requests.get('http://127.0.0.1:5000/fib', minusOnePayload)
-    with tracer.start_as_current_span("get_minus_two") as span:
-      span.set_attribute("payloadValue", value-2)
-      respTwo = requests.get('http://127.0.0.1:5000/fib', minusTwoPayload)
-    returnValue = int(respOne.content) + int(respTwo.content)
-  sys.stdout.write('\n')
-  return str(returnValue)
+    if value == 0:
+        returnValue = 0
+    elif value == 1:
+        returnValue = 1
+    else:
+        minusOnePayload = {'index': value - 1}
+        minusTwoPayload = {'index': value - 2}
+        with tracer.start_as_current_span("get_minus_one") as span:
+            span.set_attribute("payloadValue", value-1)
+            respOne = requests.get(
+                'http://127.0.0.1:5000/fib', minusOnePayload)
+        with tracer.start_as_current_span("get_minus_two") as span:
+            span.set_attribute("payloadValue", value-2)
+            respTwo = requests.get(
+                'http://127.0.0.1:5000/fib', minusTwoPayload)
+        returnValue = int(respOne.content) + int(respTwo.content)
+    sys.stdout.write('\n')
+    return str(returnValue)
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
